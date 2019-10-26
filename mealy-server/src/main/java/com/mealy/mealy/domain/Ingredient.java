ab56c6f61856deeee7395f314609c02a02311940
Emily Bowe <ebowe@tcd.ie>
2019-10-26 09:51:58 +0100

    Update Ingredient.java

--- mealy-server/src/main/java/com/mealy/mealy/domain/Ingredient.java
+++ mealy-server/src/main/java/com/mealy/mealy/domain/Ingredient.java
@@ -13,6 +13,7 @@ import javax.validation.constraints.NotBlank;
 @Entity
 @Table(name = "ingredient")
 public class Ingredient implements Serializable {
+	
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
